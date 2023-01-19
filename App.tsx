46c76937e8fd186e4bcca52183a2a1ebe8ede840
Osvaldo Costa <osvaldo.costajr@gmail.com>
2023-01-18 21:01:27 -0300

    S2L20 - Flexbox - A deep dive

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Your course goal!" style={styles.textInput} />
+        <Button title="Add goal"/>
+      </View>
+      <View>
+        <Text>List of goals</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8
+
+  }
+});
